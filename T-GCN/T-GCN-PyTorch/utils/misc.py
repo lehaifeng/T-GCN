291824f2b8bb375cc718701934ce2b16125cd456
martinwhl <asteroid02@126.com>
2021-05-26 18:13:23 +0800

    Delete misc.py

--- T-GCN/T-GCN-PyTorch/utils/misc.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-
-def format_logger(logger, format='\033[31m[%(asctime)s %(levelname)s]\033[0m%(message)s'):
-    handler = logger.handlers[0]
-    formatter = logging.Formatter(format)
-    handler.setFormatter(formatter)
